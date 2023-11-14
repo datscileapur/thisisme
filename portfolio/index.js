058b6c02805c9e722fec9fe56ae54b02ba425451
kaon <datscileapur@gmail.com>
2023-11-15 01:35:09 +0800

    updates: added a image slider ; added images; edit links;

--- portfolio/index.js
+++ portfolio/index.js
@@ -36,3 +36,39 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+
+//-- slider strat
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const images = document.querySelector('.carousel').children;
+const totalImages = images.length;
+let index = 0;
+
+prev.addEventListener('click', () => {
+  nextImage('prev');
+})
+
+next.addEventListener('click', () => {
+  nextImage('next');
+})
+
+function nextImage(direction) {
+  if(direction == 'next') {
+    index++;
+    if(index == totalImages) {
+      index = 0;
+    }
+  } else {
+    if(index == 0) {
+      index = totalImages - 1;
+    } else {
+      index--;
+    }
+  }
+
+  for(let i = 0; i < images.length; i++) {
+    images[i].classList.remove('slider-main');
+  }
+  images[index].classList.add('slider-main');
+}
